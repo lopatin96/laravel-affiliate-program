b8e0e2dd2589738fd41fd412fc5c66bdbb6e27ca
vladyslavlopatin <ilopatin3@gmail.com>
2023-11-15 02:17:56 +0100

    hasOne AffiliatePayout

--- src/Models/AffiliateInvoice.php
+++ src/Models/AffiliateInvoice.php
@@ -3,6 +3,7 @@
 namespace Atin\LaravelAffiliateProgram\Models;
 
 use App\Models\User;
+use App\Models\UserStats;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,5 +14,10 @@ class AffiliateInvoice extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function affiliatePayout()
+    {
+        return $this->hasOne(AffiliatePayout::class);
     }
 }
